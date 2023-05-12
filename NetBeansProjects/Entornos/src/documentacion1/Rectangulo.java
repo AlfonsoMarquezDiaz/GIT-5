f680e76822a30d93106ce40e42379c79ee36077d
AlfonsoMarquezDiaz <alfonsomarquezdiaz1@gmail.com>
2023-05-12 15:46:23 +0200

    Add rectangulo

---
+++ NetBeansProjects/Entornos/src/documentacion1/Rectangulo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package documentacion1;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class Rectangulo {
+    float alto,ancho;
+    public Rectangulo(float f1,float f2){
+        alto=f1;
+        ancho=f2;
+    }
+}
