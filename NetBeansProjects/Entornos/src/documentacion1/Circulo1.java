7e28ae9231b93435376e999813fd26c28caa173a
AlfonsoMarquezDiaz <alfonsomarquezdiaz1@gmail.com>
2023-05-12 15:41:37 +0200

    Añadida aplicacion

--- NetBeansProjects/Entornos/src/documentacion1/Circulo1.java
+++ NetBeansProjects/Entornos/src/documentacion1/Circulo1.java
@@ -7,7 +7,8 @@
  *
  * @author Alfonso
  * Ejercicio de Documentación
- * Circulos - Versión sin información
+ * Circulos - Versión con información
+ * @version 12/5/23
  */
 package documentacion1;
 
@@ -17,20 +18,29 @@ public class Circulo1 {
     private double centroY;
     private double radio;
 
+    /**Constructor
+     * 
+     */
     public Circulo1(double x, double y, double r) {
         centroX = x;
         centroY = y;
         radio = r;
     }
-
+    /**getter
+     * 
+     */
     public double getCentroX() {
         return centroX;
     }
-
+    /**getter
+     * 
+     */
     public double getPerimetro() {
         return 2 * Math.PI * radio;
     }
-
+    /**Desplaza los ejes
+     * 
+     */
     public void mueve(double despX, double despY) {
         centroX = centroX + despX;
         centroY = centroY + despY;
